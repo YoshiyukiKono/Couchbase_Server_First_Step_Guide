38f05c1742f6455b95bdee927ec920d2320209a8
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2021-09-09 14:46:35 +0900

    Add scripts

---
+++ app/routes.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var couchbase = require('couchbase');
+
+const cluster = new couchbase.Cluster('couchbase://localhost', {
+    username: 'user_management_app',
+    password: 'C0uchb@se',
+})
+
+const bucket = cluster.bucket('user_management');
+const collection = bucket.scope('japan').collection('users')
+
+const qs = 'SELECT id, name from user_management.japan.users'; 
+
+const upsertDocument = async (doc) => {
+  try {
+    const key = `${doc.id}`;
+    const result = await collection.upsert(key, doc);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const removeUser = async (id) => {
+  try {
+    const key = `user_${id}`;
+    const result = await collection.remove(key);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const selectUsers = async (key) => { 
+  const result = await cluster.query(qs, {
+    scanConsistency: couchbase.QueryScanConsistency.RequestPlus,
+  });
+  return result.rows;
+}
+
+var routes = function(app) {
+    app.get('/users', async (req, res) => {      
+        const rows = await selectUsers();
+        res.json(rows);
+      });
+    app.post('/user', function(req, res) {
+      const user = {
+        id: req.body.id,
+        name: req.body.name,
+      };
+      upsertDocument(user);
+      res.json({});
+    });
+    app.delete("/user/:id", function(req, res) {
+      removeUser(req.query.id);
+      res.json({});
+  });
+};
+module.exports = routes;
